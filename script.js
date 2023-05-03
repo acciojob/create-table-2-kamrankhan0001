067a69771b1f051a4f7e8c4d368774e23ffd1563
MD KAMRAN KHAN <124767601+kamrankhan0001@users.noreply.github.com>
2023-05-03 20:25:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function createTable() {
+//We use the parseInt() function to convert the user input 
+//into integers, which we store in the rows and cols variables.
 const rows = parseInt(prompt('Input number of rows'));
 const cols = parseInt(prompt('Input number of columns'));
 const table = document.getElementById('myTable');
