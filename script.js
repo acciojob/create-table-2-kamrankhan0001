9518768c37e5c60f345eabfe7edc13272533643b
MD KAMRAN KHAN <124767601+kamrankhan0001@users.noreply.github.com>
2023-05-03 20:21:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 function createTable() {
-    const rows = parseInt(prompt('Input number of rows'));
-  const cols = parseInt(prompt('Input number of columns'));
-
-  const table = document.getElementById('myTable');
+const rows = parseInt(prompt('Input number of rows'));
+const cols = parseInt(prompt('Input number of columns'));
+const table = document.getElementById('myTable');
 
   for (let i = 0; i < rows; i++) {
     const row = table.insertRow(i);
@@ -10,6 +9,5 @@ function createTable() {
       const cell = row.insertCell(j);
       cell.innerHTML = `Row-${i} Column-${j}`;
     }
-  }
-	
+  }	
 }
